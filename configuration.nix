52b379391be44fc5478e64ab8276acd7e4a07b2a
ClientCrash <client@clientcrash.com>
2024-07-23 19:49:32 +0200

    basic

---
+++ configuration.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  # Bootloader configuration
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  # Networking configuration
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  # Time and locale settings
+  time.timeZone = "Europe/Berlin";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Xserver configuration
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Docker configuration
+  virtualisation.docker.enable = true;
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+  users.extraGroups = [ "docker" ];
+
+  # User configuration
+  users.users.noah = {
+    isNormalUser = true;
+    description = "noah";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [ ];
+  };
+
+  # SSH configuration
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "no";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    neofetch
+    htop
+    pastebinit
+  ];
+
+  system.stateVersion = "24.05";
+}
