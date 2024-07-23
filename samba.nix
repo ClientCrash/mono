d4df6dbde920f249ec16871542cde001fbe7b882
ClientCrash <client@clientcrash.com>
2024-07-23 21:27:18 +0200

    Update samba.nix

--- samba.nix
+++ samba.nix
@@ -1,24 +1,34 @@
-{ config, pkgs, ... }:
+services.samba = {
+  enable = true;
+  securityType = "user";
+  openFirewall = true;
+  extraConfig = ''
+    workgroup = WORKGROUP
+    server string = smbnix
+    netbios name = smbnix
+    security = user 
+    #use sendfile = yes
+    #max protocol = smb2
+    guest account = nobody
+    map to guest = bad user
+  '';
+  shares = {
+    public = {
+      path = "/mnt/pool/share";
+      browseable = "yes";
+      "read only" = "no";
+      "guest ok" = "yes";
+      "create mask" = "0644";
+      "directory mask" = "0755";
+      "force user" = "username";
+      "force group" = "groupname";
+    };
+};
 
-{
+services.samba-wsdd = {
+  enable = true;
+  openFirewall = true;
+};
 
-  # Ensure the share directory exists
-  environment.etc."mnt/pool/share" = {
-    text = "";
-  };
-
-  services.samba = {
-    package = pkgs.samba4Full;
-    enable = true;
-    openFirewall = true;
-    extraConfig = ''
-      [pool]
-      path = /mnt/pool/share
-      browseable = yes
-      writable = yes
-      guest ok = no
-      create mask = 0644
-      directory mask = 0755
-    '';
-  };
-}
+networking.firewall.enable = true;
+networking.firewall.allowPing = true;
