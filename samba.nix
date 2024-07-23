04eb1f832e9b622392997ac8ff88d657a77c0017
ClientCrash <client@clientcrash.com>
2024-07-23 21:29:44 +0200

    Update samba.nix

--- samba.nix
+++ samba.nix
@@ -1,38 +1,39 @@
 { config, pkgs, ... }:
 
-
+{
   services.samba = {
-  enable = true;
-  securityType = "user";
-  openFirewall = true;
-  extraConfig = ''
-    workgroup = WORKGROUP
-    server string = smbnix
-    netbios name = smbnix
-    security = user 
-    #use sendfile = yes
-    #max protocol = smb2
-    guest account = nobody
-    map to guest = bad user
-  '';
-  shares = {
-    public = {
-      path = "/mnt/pool/share";
-      browseable = "yes";
-      "read only" = "no";
-      "guest ok" = "yes";
-      "create mask" = "0644";
-      "directory mask" = "0755";
-      "force user" = "username";
-      "force group" = "groupname";
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = smbnix
+      netbios name = smbnix
+      security = user
+      #use sendfile = yes
+      #max protocol = smb2
+      guest account = nobody
+      map to guest = bad user
+    '';
+    shares = {
+      public = {
+        path = "/mnt/pool/share";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "username";
+        "force group" = "groupname";
+      };
     };
-};
+  };
 
-services.samba-wsdd = {
-  enable = true;
-  openFirewall = true;
-};
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
 
-networking.firewall.enable = true;
-networking.firewall.allowPing = true;
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
 }
